506003186e026720131e5f8ee42648bc2976b11a
Sebastian <jsebasnd1103@gmail.com>
2025-09-15 10:29:25 -0500

    Blockchain

---
+++ BlockChain.js
@@ -0,0 +1,55 @@
+
+// Hash simple para navegador (no seguro, solo demostración)
+function simpleHash(str) {
+    let hash = 0, i, chr;
+    if (str.length === 0) return hash;
+    for (i = 0; i < str.length; i++) {
+        chr   = str.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0;
+    }
+    return hash.toString();
+}
+
+class Block {
+    constructor(index, data, previousHash = "") {
+        this.index = index;
+        this.date = new Date().toISOString();
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.createHash();
+    }
+    createHash() {
+        return simpleHash(this.index + this.date + JSON.stringify(this.data) + this.previousHash);
+    }
+}
+
+class Blockchain {
+    constructor(genesis) {
+        this.chain = [this.createFirstBlock(genesis)];
+    }
+    createFirstBlock(data) {
+        return new Block(0, data);
+    }
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(data) {
+        let prevBlock = this.getLastBlock();
+        let block = new Block(prevBlock.index + 1, data, prevBlock.hash);
+        this.chain.push(block);
+    }
+    isValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            let current = this.chain[i];
+            let prev = this.chain[i - 1];
+            if (current.hash !== current.createHash()) return false;
+            if (current.previousHash !== prev.hash) return false;
+        }
+        return true;
+    }
+}
+
+// Exponer las clases globalmente
+window.Block = Block;
+window.Blockchain = Blockchain;
